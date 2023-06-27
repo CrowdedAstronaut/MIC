721c8d489043f1bc723cdfd680f88d05f8abdaeb
ross phillips <ross@johnrossphillips.com>
2023-06-27 14:37:16 -0500

    acc showcase

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const gameContainer = document.querySelector(".game-container");
 
 // Get all elements with an ID
 const elementsWithId = document.querySelectorAll("[id]");
-
+console.log(elementsWithId);
 // Loop through each element with an ID
 elementsWithId.forEach((element) => {
   // Add a mouseover event listener to each element
@@ -12,8 +12,8 @@ elementsWithId.forEach((element) => {
     const hoveredElementId = element.id;
 
     // Construct the URL of the background image based on the ID
-    const backgroundImageUrl = `.https://github.com/CrowdedAstronaut/MIC/blob/main/assets/${hoveredElementId}.svg`;
-
+    const backgroundImageUrl = `https://github.com/CrowdedAstronaut/MIC/blob/main/assets/${hoveredElementId}.svg`;
+    console.log(backgroundImageUrl);
     // Set the background image of the .game-container
     gameContainer.style.backgroundImage = `url(${backgroundImageUrl})`;
   });
